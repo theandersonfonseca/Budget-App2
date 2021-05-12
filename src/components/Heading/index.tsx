782bf71daeab768b4a486b6ab241fff2f0f61779
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-11 21:02:39 -0300

    add Heading component

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+const Heading = () => {
+  return (
+    <S.Wrapper>Orçamento</S.Wrapper>
+  )
+}
+
+export default Heading
---
+++ src/components/Heading/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.h1`
+  ${({theme}) => css`
+    font-size: ${theme.sizes.huge};
+    text-transform: uppercase;
+    font-weight: ${theme.font.normal};
+    color: ${theme.colors.secondary};
+    letter-spacing: calc(${theme.spacings.xxsmall } / 2);
+  `}
+`
