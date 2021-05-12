c624ef72a5fe8e540281cce941945c2935c63db0
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-12 19:04:56 -0300

    add isInputValid function

---
+++ src/utils/isInputValid.ts
@@ -0,0 +1,18 @@
+import { ListType } from "../state/types";
+
+export default function isInputValid(descriptionInput: string, amountInput: string, list: ListType[]) {
+  if (descriptionInput.trim().length === 0 || amountInput.trim().length === 0) {
+    window.alert('Dados Inválidos!')
+
+    return false
+  }
+
+  if (list.some(income => income.description === descriptionInput)) {
+    window.alert('Essa Descrição já Existe!')
+
+    return false
+  }
+
+
+  return true
+}
