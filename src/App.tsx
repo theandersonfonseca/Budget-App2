2a3dbc844c191ef00f83d073342741f4094e75f0
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-12 19:07:17 -0300

    add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import {ContextProvider} from './state/context'
 
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 import theme from './styles/theme';
 
+import Container from './components/Container';
+import Heading from './components/Heading';
+import Results from './components/Results';
+import Inputs from './components/Inputs';
+import Histories from './components/Histories';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <ContextProvider>
+        <Container>
+          <div>
+            <Heading/>
+            <Results/>
+          </div>
+
+          <div>
+            <Inputs/>
+            <Histories/>
+          </div>
+        </Container>
+      </ContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   );
